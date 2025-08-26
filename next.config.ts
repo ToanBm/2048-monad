52e98b35b4204a2989e14dd6ab91227c75820a52
ToanBm <buiminhtoan1985@gmail.com>
2025-08-26 12:37:49 +0700

    Fix scaling issues and reduce border sizes

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_PRIVY_APP_ID: "cmeb4lcu60030js0crxx422ro",
     NEXT_PUBLIC_MONAD_APP_ID: "cmd8euall0037le0my79qpz42",
     NEXT_PUBLIC_MONAD_PORTAL_URL: "https://monad-games-id-site.vercel.app",
-    NEXT_PUBLIC_API_BASE_URL: "https://your-vps-ip:3001",
+    NEXT_PUBLIC_API_BASE_URL: "http://172.20.144.16:3001",
     NEXT_PUBLIC_GAME_ADDRESS: "0xc62cc8d9cf9186f5f1e6458641b45c70c1899537",
   },
 };
